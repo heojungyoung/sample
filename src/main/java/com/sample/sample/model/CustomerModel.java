159cfb7236a8baf279194f5c8d4a5fc022aec5b8
hjy <tyireal@gmail.com>
2021-04-03 20:12:12 +0900

    order page
    order page

---
+++ src/main/java/com/sample/sample/model/CustomerModel.java
@@ -0,0 +1,21 @@
+package com.sample.sample.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CustomerModel {
+
+	private String CustomerID;
+	private String CustFirstName;
+	private String CustLastName;
+	private String CustStreetAddress;
+	private String CustCity;
+	private String CustState;
+	private String CustZipCode;
+	private String CustAreaCode;
+	private String CustPhoneNumber;
+	
+	
+}

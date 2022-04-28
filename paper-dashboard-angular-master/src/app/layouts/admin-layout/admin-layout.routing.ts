4c7fe720141be87475b47a12643f6c6b91ea0912
tyireal <tyireal@gmail.com>
2022-04-28 17:16:39 +0900

    map 제거

--- paper-dashboard-angular-master/src/app/layouts/admin-layout/admin-layout.routing.ts
+++ paper-dashboard-angular-master/src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -5,7 +5,6 @@ import { UserComponent } from '../../pages/user/user.component';
 import { TableComponent } from '../../pages/table/table.component';
 import { TypographyComponent } from '../../pages/typography/typography.component';
 import { IconsComponent } from '../../pages/icons/icons.component';
-import { MapsComponent } from '../../pages/maps/maps.component';
 import { NotificationsComponent } from '../../pages/notifications/notifications.component';
 import { UpgradeComponent } from '../../pages/upgrade/upgrade.component';
 import { OrderComponent } from '../../pages/order/order.component';
@@ -16,7 +15,6 @@ export const AdminLayoutRoutes: Routes = [
     { path: 'table',          component: TableComponent },
     { path: 'typography',     component: TypographyComponent },
     { path: 'icons',          component: IconsComponent },
-    { path: 'maps',           component: MapsComponent },
     { path: 'notifications',  component: NotificationsComponent },
     { path: 'upgrade',        component: UpgradeComponent },
     { path: 'order',        component: OrderComponent }
